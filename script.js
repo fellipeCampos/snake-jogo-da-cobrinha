bd936e66154e21c6ef74e3209b5d825c9728f4c4
fellipeCampos <fellipe.integrador@gmail.com>
2021-10-08 20:19:33 -0300

    criando direções

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ snake[0] = {
     y: 8 * box  
 
 }
+let direction = "right";
+
 function criarBG() { /* desenhar e definir cor */
     context.fillStyle = "lightgrenn";
     context.fillRect(0, 0, 16 * box, 16 * box); /* retangulo, onde vai acontecer o jogo */
@@ -14,11 +16,31 @@ function criarBG() { /* desenhar e definir cor */
 
 function criarCobrinha(){
     for(i=0; i < snake.length; i++){
-        context.fillStyle = "red"
-        context.fillRect(snake[i].y, snake[i].y, box, box);
+        context.fillStyle = "grenn"
+        context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
-criarBG();
-criarCobrinha();
+function iniciarJogo(){
+    criarBG();
+    criarCobrinha();
 
+    let snakex = snake[0].x;
+    let snakey = snake[0].y;
+
+    if(direction == "right") snakex += box;
+    if(direction == "left") snakex -= box;
+    if(direction == "up") snakey -= box;
+    if(direction == "down") snakey += box;
+
+    snake.pop();
+    let newHead = {
+        x: snakex,
+        y: snakey
+    }
+
+    snake.unshift(newHead);
+
+}
+
+let jogo = setInterval(iniciarJogo, 100);/* ms para iniciar o jogo */
